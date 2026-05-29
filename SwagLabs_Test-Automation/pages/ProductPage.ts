c7660be6adacaad804747d730d150c832eee1f53
smsc07 <shiela.cuello13@outlook.com>
2026-05-29 20:45:39 +0800

    feat(product): add test cases for e2e wip

--- SwagLabs_Test-Automation/pages/ProductPage.ts
+++ SwagLabs_Test-Automation/pages/ProductPage.ts
@@ -1,24 +1,79 @@
-import { Page } from '@playwright/test';
-//import { test } from '../fixtures/testFixtures.ts';
+import { Page, Locator, expect } from '@playwright/test';
 import { testData } from '../test-data/testData';
 
 export class ProductPage{
 
-    private page:Page;
-
-    constructor(page:Page){
-        this.page = page;
-    }
+    private page: Page;
+    readonly inventoryList: Locator;
+    readonly inventoryItem: Locator;
+    readonly inventoryItemImg: Locator;
+    readonly inventoryItemDes: Locator;
 
     // Locators
     productItems = '[data-test="inventory-item"]';
 
-    async getProductCount(): Promise<number> {
-        return await this.page.locator(this.productItems).count();
+    addedItems: string[] = [];
+    
+
+    private removeBtnCSS = 'btn btn_secondary btn_small btn_inventory ';
+
+    constructor(page:Page){
+        this.page = page,
+        this.inventoryList = this.page.locator('[data-test="inventory-list"]');
+        this.inventoryItem = this.page.locator('[data-test="inventory-item"]');
+        this.inventoryItemImg = this.page.locator('[data-test="inventory-item_img"]');
+        this.inventoryItemDes = this.page.locator('[data-test="inventory-item-description"]');
+        this.addedItems = [];
     }
 
     async navigateToProductsPage() {
-        return await this.page.goto(testData.base_url + '/inventory.html')
+        return await this.page.goto(testData.base_url + '/inventory.html');
+    }
+
+    async itemImagesVisibilityCheck() {
+        const items = this.page.locator('[data-test^="item-"][data-test$="-img-link"]');
+        const count = await items.count();
+
+        for (let i = 0; i < count; i++) {
+            await expect(items.nth(i)).toBeVisible();
+        }
+    }
+
+    async itemDesCountAndVisibilityCheck() {
+
+        const count = await this.inventoryItemDes.count();
+        await expect(this.inventoryItemDes).toHaveCount(count);
+
+        //for loop to check the item description one-by-one
+        for (let i = 0; i < count; i++) {
+            await expect(this.inventoryItemDes.nth(i)).toBeVisible();
+        };
+    }
+
+    async addItem(addToCartBtn: string, removeToCartBtn:string){
+
+        //Add item to array
+        this.addedItems.push(addToCartBtn)
+
+        //click add to cart button
+        return await expect(this.page.locator(addToCartBtn)).toBeVisible(),
+        await this.page.locator(addToCartBtn).click(),
+
+        //check button change
+        await expect(this.page.locator(removeToCartBtn)).toBeVisible(),
+        await expect(this.page.locator(removeToCartBtn)).toHaveClass(this.removeBtnCSS);
+    }
+
+    async countShoppingCartItem(addedItems:number){
+
+    }
+
+        await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
+
+
+
+    async getProductCount(): Promise<number> {
+        return await this.page.locator(this.productItems).count();
     }
 
 }
---
+++ SwagLabs_Test-Automation/tests/e2e/e2e.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '../../fixtures/testFixtures';
+import { testData } from '../../test-data/testData';
+import { takeScreenshot, takeSnip } from '../../helpers/screenshotHelper';
+import { count } from 'node:console';
+
+test('E2E-001', async ({ loginPage, productPage, cartPage, page }) => {
+
+    //onboarding
+    await loginPage.navigate();
+    await loginPage.login(
+                testData.validUser.username,
+                testData.validUser.password,
+    );
+
+    //assert product page
+    await productPage.navigateToProductsPage();
+    await expect(productPage.inventoryList).toBeVisible();
+    await expect(productPage.inventoryItem).toHaveCount(6);
+    await productPage.itemImagesVisibilityCheck();
+    await productPage.itemDesCountAndVisibilityCheck();
+    await takeScreenshot(page, 'E2E-001 - ER1');
+
+
+    //add item 1
+    await productPage.addItem('[data-test="add-to-cart-sauce-labs-backpack"]',
+        '[data-test="remove-sauce-labs-backpack"]');
+    //add item 2
+    await productPage.addItem('[data-test="add-to-cart-sauce-labs-onesie"]',
+        '[data-test="remove-sauce-labs-onesie"]');
+    //add item 3
+    await productPage.addItem('[data-test="add-to-cart-sauce-labs-fleece-jacket"]',
+        '[data-test="remove-sauce-labs-fleece-jacket"]');
+    await takeScreenshot(page, 'E2E-001 - ER2');
+
+    
+    //ER3
+
+    
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
+
+    await count(page.)
+
+    await takeScreenshot(page, 'E2E-001 - ER3');
+
+
+    await console.log(productPage.addedItems);
+
+
+    
+});
